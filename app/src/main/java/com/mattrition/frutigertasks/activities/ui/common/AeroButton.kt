db34dcc9dcf3b9095b1a33ce61e21d8e75835a5a
Matthew Cumbo <mattcumbo99@gmail.com>
2025-06-12 11:09:36 -0500

    Asset improvements and additions (#4)
    * Random XML changes
    
    * Deleted example tests
    
    * Added spotless
    
    * Formatting
    
    * Various improvements
    
    * Various improvements 2
    
    * Asset improvements

--- app/src/main/java/com/mattrition/frutigertasks/activities/ui/common/AeroButton.kt
+++ app/src/main/java/com/mattrition/frutigertasks/activities/ui/common/AeroButton.kt
@@ -1,66 +1,63 @@
 package com.mattrition.frutigertasks.activities.ui.common
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.mattrition.frutigertasks.activities.ui.theme.FrutigerTasksTheme
+import androidx.compose.ui.unit.em
+import com.mattrition.frutigertasks.R
+import com.mattrition.frutigertasks.activities.ui.theme.DenimBlue
+import com.mattrition.frutigertasks.activities.ui.theme.RoyalBlue
 
 @Composable
-fun AeroButton(
-    text: String,
-    colors: List<Color>,
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier
-) {
-    Button(onClick = onClick, colors = ButtonDefaults.buttonColors(Color.Transparent)) {
-        Box(
-            modifier =
-            Modifier.background(
-                brush = Brush.verticalGradient(colors),
-                shape = RoundedCornerShape(15.dp)
+fun AeroButton(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Box(
+        modifier =
+        Modifier.padding(5.dp)
+            .clickable { onClick }
+            .background(
+                brush = Brush.verticalGradient(listOf(Color.White, RoyalBlue, DenimBlue)),
+                shape = RoundedCornerShape(50.dp)
             )
-                .padding(horizontal = 20.dp, vertical = 5.dp)
-                .then(modifier)
-        ) {
-            Text(text = text)
-        }
+            .padding(15.dp)
+            .then(modifier),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = text, fontSize = 5.em, color = Color.White)
     }
 }
 
 @Composable
-fun AeroAcceptButton(onClick: () -> Unit, modifier: Modifier = Modifier, text: String = "Accept") =
-    AeroButton(text, listOf(Color.LightGray, Color.Green), onClick, modifier)
-
-@Composable
-fun AeroDeclineButton(
-    onClick: () -> Unit,
-    modifier: Modifier = Modifier,
-    text: String = "Decline"
-) = AeroButton(text, listOf(Color.LightGray, Color.Red), onClick, modifier)
-
-@Composable
-fun AeroInfoButton(onClick: () -> Unit, modifier: Modifier = Modifier, text: String = "Info") =
-    AeroButton(text, listOf(Color.LightGray, Color.Blue), onClick, modifier)
+fun AeroCheckmarkButton(onClick: () -> Unit) {
+    Image(
+        painter = painterResource(id = R.drawable.check_button),
+        contentDescription = "Checkmark box",
+        modifier = Modifier.padding(5.dp).clickable { onClick }
+    )
+}
 
 @Composable
 @Preview(showBackground = true)
 private fun AeroButtonPreview() {
-    FrutigerTasksTheme {
-        Column {
-            AeroAcceptButton({})
-            AeroDeclineButton({})
-            AeroInfoButton({})
-        }
+    ScreenBuilder(
+        screenTitle = "Simple buttons",
+        mainScreen = true,
+        actions = { AeroCheckmarkButton {} }
+    ) {
+        AeroButton(text = "Add", onClick = {})
+
+        AeroButton(text = "Add", onClick = {}, modifier = Modifier.fillMaxWidth())
     }
 }
